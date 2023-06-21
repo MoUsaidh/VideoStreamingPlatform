dc7531eb52766b7e88a0aa0ab18050643e70b5f9
MoUsaidh <usaidhsuhail@gmail.com>
2023-06-21 16:40:04 +0530

    added new file for checking

--- API/main.py
+++ API/main.py
@@ -7,6 +7,11 @@ app = FastAPI()
 def root():
     return {"Hello": "Worlds"}
 
+
 @app.get("/search")
 def search():
-    return {"search": "result"}
+    return {"search": "result as json"}
+
+    # get the push request
+    # search the csv file
+    # return the result as json
